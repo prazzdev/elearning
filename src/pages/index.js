59af2bf14acc68530442a768622d43931aa677e6
prazzdev <prazzid31@gmail.com>
2023-07-21 02:37:28 +0700

    modified:   src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import Header from '@/components/Header'
 import Main from '@/components/Main'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>E-Learning API Docs</title>
+    </Head>
     <Header />
     <Main />
     </>
